6049f37c2bcad437d21d73ce6e561f89a3d7b3f9
Alexey Malkov <54933718+MarloweWind@users.noreply.github.com>
2023-04-06 12:50:31 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Marlowe Wind on 06.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
